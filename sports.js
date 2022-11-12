ba9d3491d24b119ab1f44c48ab4048a66e262520
AdrianoBalbo <adriano.balbo00@gmail.com>
2022-11-12 00:09:54 -0300

    avances en sports

---
+++ sports.js
@@ -0,0 +1,9 @@
+const section1 = document.querySelector(".section1");
+const section2 = document.querySelector(".section2");
+const section3 = document.querySelector(".section3");
+const section4 = document.querySelector(".section4");
+
+
+// function openTab() {
+//     cla
+// }
